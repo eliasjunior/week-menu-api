11b20fde6bdba746273d4b0041c85a1e98a5791a
Elias Moreira <elias_moreirajunior@mastercard.com>
2016-11-27 17:00:12 +0000

    add more stuff

--- models/ingredient.model.js
+++ models/ingredient.model.js
@@ -5,4 +5,51 @@
 (function () {
     'use strict'
 
+    var mongoose = require('mongoose');
+
+    var Schema = mongoose.Schema;
+
+    exports.recipe = function() {
+        //
+        // weekDay: string;
+        // menus: MenuHistory [];
+        // categories : Category[]; //to display
+        // ingredient_ids: string [];
+        // mainMealValue: string;
+        // mainMeal: any;
+        // countIngredient: number;
+        // description: string;
+
+        var recipeSchema = new Schema({
+            name: {
+                type: String,
+                minlength: 1,
+                trim: true
+            },
+            weekDay: String,
+            checked: {
+                type: Boolean,
+                default: false
+            },
+            mainMeal : {
+                name : {
+                    type: String,
+                    minlength: 1,
+                    trim: true
+                },
+                label : String,
+                icon: String
+            },
+            countIngredient: Number,
+            description: String
+        });
+
+        var recipesSchema = new Schema({
+            recipes : [recipeSchema]
+        })
+
+        return mongoose.model('Recipe', recipesSchema, 'recipes');
+    }
+
+
 })();
