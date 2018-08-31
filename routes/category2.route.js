00807564da485c6f1421b4388b0d9169b6f80869
Elias Moreira <elias_moreirajunior@mastercard.com>
2018-08-31 13:27:45 +0100

    new route category

--- routes/category2.route.js
+++ routes/category2.route.js
@@ -9,7 +9,21 @@ function getCategoryRoute(request, response) {
         .then(doc => responseHandlerService.send(response, {doc, status: STATUS.GET_CODE}))
         .catch(reason => responseHandlerService.error(response, reason));
 }
+function save(request, response) {
+    CategoryService
+        .save(request.body)
+        .then(doc => responseHandlerService.send(response, {doc, status: STATUS.CREATE_CODE}))
+        .catch(reason => responseHandlerService.error(response, reason));
+}
+function update(request, response) {
+    CategoryService
+        .update(request.body)
+        .then(() => responseHandlerService.send(response, {status: STATUS.UPDATE_CODE}))
+        .catch(reason => responseHandlerService.error(response, reason));
+}
 
-router.get("/category", getCategoryRoute);
+router.get('/category', getCategoryRoute);
+router.post('/category', save);
+router.put('/category', update);
 
 module.exports = router;
--- services/category.service.js
+++ services/category.service.js
@@ -1,12 +1,30 @@
-const CategoryService = () => {
-    const { Category } = require('../models/category.model');
+const { Category } = require('../models/category.model');
+const ProductValidation = require('../services/product.validation');
 
+function CategoryService() {
     return {
         save(category) {
-
+            const categoryModel = new Category({
+                name: category.name,
+                insertDate: new Date(),
+            });
+            return categoryModel.save()
+                .then(doc => doc)
+                .catch(reason => {
+                    return Promise
+                        .reject(ProductValidation.messageValidation(reason));
+                });
         },
         update(category) {
-
+            return Category
+                .findOne({_id: category._id})
+                .then(doc => {
+                    doc.name = category.name;
+                    return doc.save();
+                }).catch(reason => {
+                    return Promise
+                        .reject(ProductValidation.messageValidation(reason));
+                });
         },
         addProduct(id, ingredient) {
             return Category.findOne({ _id: id })
