eafbad4ce52eaed6cc609e707944d73f4d557e35
Elias Moreira <elias_moreirajunior@mastercard.com>
2018-09-14 16:15:04 +0100

    add files

--- services/migrateService.js
+++ services/migrateService.js
@@ -1,7 +1,9 @@
 const { Category } = require('../models/category.model');
 
-const categoryFile = require('../db_backup/categories.json')
+const categoryFile = require('../db_backup/categories')
 
 function migration() {
+    console.log('*** *** *** ** ** ')
+    console.log('READING FILE ', categoryFile)
 
 }
